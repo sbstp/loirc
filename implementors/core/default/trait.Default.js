a1e95c25db247a97c939e61abc83510ff6719070
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-07-28 16:06:57 -0400

    Update docs

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['irc'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='irc/enum.ReconnectionSettings.html' title='irc::ReconnectionSettings'>ReconnectionSettings</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
