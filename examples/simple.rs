2e486e6115ff1e9f76e1eed61384a4481f1ffee6
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-07-28 13:41:57 -0400

    Refresh example

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,31 +3,22 @@ extern crate irc;
 use std::thread;
 
 fn main() {
-    let (writer, reader) = irc::connect("irc.freenode.net:6667").unwrap();
-    writer.user("scarlet", "scarlet");
-    writer.nick("scarlet-irc");
+    // Connect to freenode and use default reconnection settings.
+    let (writer, reader) = irc::connect("irc.freenode.net:6667", None).unwrap();
+    writer.user("myuser", "my real name");
+    writer.nick("mynick");
 
-    let pinger = writer.clone();
-    thread::spawn(move || {
-        thread::sleep_ms(15 * 1000);
-        pinger.raw("PING irc.freenode.net\n".into());
-        println!("sent ping");
-    });
-
+    // Receive events.
     for event in reader.iter() {
         println!("{:?}", event);
         match event {
             irc::Event::Message(msg) => {
                 if msg.code == irc::Code::RplWelcome {
-                    writer.close();
+                    // join #mychannel, no password
+                    writer.join("#mychannel", None);
                 }
-                //println!("{:?}", msg);
-            }
-            irc::Event::ParseError(err) => {
-                println!("{:?}", err);
             }
             _ => {}
         }
     }
-    println!("we done heere");
 }
