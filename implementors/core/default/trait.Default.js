7797ea84ed860a90bdd6acf5464bce92eb0fbc7b
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-07-30 14:48:06 -0400

    Update docs

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['irc'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='irc/enum.ReconnectionSettings.html' title='irc::ReconnectionSettings'>ReconnectionSettings</a>",];
+implementors['irc'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='irc/struct.MonitorSettings.html' title='irc::MonitorSettings'>MonitorSettings</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='enum' href='irc/enum.ReconnectionSettings.html' title='irc::ReconnectionSettings'>ReconnectionSettings</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- irc/util/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["ActivityMonitor",""]]});
