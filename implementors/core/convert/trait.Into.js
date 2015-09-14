2ec3a1b5381dfae4c27a54c11e5208492bc9fd9f
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-09-14 16:55:02 -0400

    Update docs

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['loirc'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='enum' href='loirc/enum.Event.html' title='loirc::Event'>Event</a>&gt; for <a class='enum' href='http://doc.rust-lang.org/nightly/core/result/enum.Result.html' title='core::result::Result'>Result</a>&lt;<a class='struct' href='loirc/struct.Message.html' title='loirc::Message'>Message</a>, <a class='enum' href='loirc/enum.ParseError.html' title='loirc::ParseError'>ParseError</a>&gt;",];
+implementors['loirc'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='enum' href='loirc/enum.Event.html' title='loirc::Event'>Event</a>&gt; for <a class='enum' href='https://doc.rust-lang.org/nightly/core/result/enum.Result.html' title='core::result::Result'>Result</a>&lt;<a class='struct' href='loirc/struct.Message.html' title='loirc::Message'>Message</a>, <a class='enum' href='loirc/enum.ParseError.html' title='loirc::ParseError'>ParseError</a>&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
